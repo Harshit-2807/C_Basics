13f1706aabacce31a43a3b9a69761f78f6a7481c
Harshit <harshit0580.be21@chitkara.edu.in>
2023-01-10 12:03:06 +0530

    Day 2

--- calculator.c
+++ calculator.c
@@ -8,19 +8,38 @@ int main()
     printf("D. Divide\n");
     printf("Enter your choice\n");
     scanf("%c",&a);
+    // scanf("%d",&a);
     printf("Enter variables: \n");
     scanf("%d",&b);
     scanf("%d",&c);
-    if(a=='A' || a=='a' || a=='+')
-        t=b+c;
-    if(a=='S' || a=='s' || a=='-')
-        t=b-c;
-    if(a=='M' || a=='m' || a=='*')
-        t=b*c;
-    if(a=='D' || a=='d' || a=='/')
-        t=b/c;
-    // else
-    //     printf("Wrong choice\n");
+    // if(a=='A' || a=='a' || a=='+')
+    //     t=b+c;
+    // if(a=='S' || a=='s' || a=='-')
+    //     t=b-c;
+    // if(a=='M' || a=='m' || a=='*')
+    //     t=b*c;
+    // if(a=='D' || a=='d' || a=='/')
+    //     t=b/c;
+    // // else
+    // //     printf("Wrong choice\n");
+
+    switch(a)
+    {
+        case 'A':
+            t=b+c;
+            break;
+        case 'S':
+            t=b-c;
+            break;
+        case 'M':
+            t=b*c;
+            break;
+        case 'D':
+            t=b/c;
+            break;
+        default:
+            printf("Wrong choice");
+    }
     printf("Result %d",t);
     return 0;
 }
--- vowel.c
+++ vowel.c
@@ -4,9 +4,29 @@ int main()
     char a;
     printf("Enter character :\n");
     scanf("%c",&a);
-    if(a=='A'||a=='a'||a=='E'||a=='e'||a=='I'||a=='i'||a=='O'||a=='o'||a=='U'||a=='u')
-        printf("Vowel \n");
-    else
-        printf("Consonant \n");
-    return 0;
+    // if(a=='A'||a=='a'||a=='E'||a=='e'||a=='I'||a=='i'||a=='O'||a=='o'||a=='U'||a=='u')
+    //     printf("Vowel \n");
+    // else
+    //     printf("Consonant \n");
+    // return 0;
+    switch(a)
+    {
+        case 'A':
+            printf("Vowel");
+            break;
+        case 'E':
+            printf("Vowel");
+            break;
+        case 'I':
+            printf("Vowel");
+            break;
+        case 'O':
+            printf("Vowel");
+            break;
+        case 'U':
+            printf("Vowel");
+            break;
+        default:
+            printf("Consonant");
+    }
 }
